4be90f20e3fd756e9810a3779d49b687b9cbe6dc
Abbey Feuka <46827256+afeuka@users.noreply.github.com>
2020-10-06 20:08:11 -0600

    loop de loop

--- Intermediate/looping.R
+++ Intermediate/looping.R
@@ -30,28 +30,41 @@ for(i in 1:10){
 
 # This is usful when you want to automatatically update a vector
 v <- numeric(10) # create a vector of 10 0's
+v
 for(i in 1:10){
   v[i] <- i
 }
 v
 
+# for loops are usful for iterative operations
 running.mean <- numeric(0)
 for(i in 1:10){
   running.mean[i] <- mean(1:i)
 }
 running.mean
 
+# Loops are useful for random walks!
+xy <- matrix(data=0,nrow=10,ncol=2) #columns = coordinates
+for(t in 2:10){
+  xy[t,] <- rnorm(2,xy[t-1,],0.5)
+}
+
+plot(xy[,1],xy[,2])
+lines(xy[,1],xy[,2])
+
 ###
 ### Apply functions
 ###
 
-# Similar to for loops, but condensed code for matrices
+# Similar to for loops, but condensed code and a bit less flexible
 
+# apply() is used for matrices
 # apply(OBJECT, MARGIN (1=row, 2=colum), FUNCTION)
 mat <- matrix(data=c(rep(1,4),rep(2,4),rep(3,4)),nrow=4,ncol=3)
 apply(mat, 1, sum)
 apply(mat, 2, prod)
 
+# sapply() can be used for matrices, vectors, or arrays
 vec <- 1:10
 sapply(vec, function(x){x+1})
 
@@ -59,8 +72,15 @@ sapply(vec, function(x){x+1})
 ####  Exercises ####
 #####################
 
-# 1.)  
+# 1.) Fix the following code so that it runs:
+y <- numeric(0)
+for(1:10){
+  y[i] <- mean(1:i)
+}
 
-# 2.) 
+# 2.) Sum each the column in the following matrix:
+prac <- matrix(rnorm(20,10,4),4,5)
 
-# 3.) 
+# 3.) Create a vector where each value is the previous value squared, starting with 2.
+
+
