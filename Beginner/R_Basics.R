b38f88140b2934c04b546ae8c51e8efc5873867e
Abbey Feuka <46827256+afeuka@users.noreply.github.com>
2020-12-01 11:27:38 -0700

    ABBEY IS COMMITTING

--- Beginner/R_Basics.R
+++ Beginner/R_Basics.R
@@ -2,12 +2,13 @@
 #### Module 1: R Basics
 #### Author: Abbey Feuka
 
-
 ########################
 # Notes:
 #   1. To run code, click the line you want to run and click "Run", or CTRL + r (PC) or Command + r (mac)
-#   2. Object names can include letters, numbers, '.', and '_' but must START with a letter (and are case-sensitive)
+#   2. Object names can include letters, numbers, '.', and '_' but must START with a letter 
+#   2.a. Object names are case-sensitive!
 #   3. Use the 'Help' tab to look up the documentation for any function
+#   4. Starting a line of code with # creates a comment, or non-run-able code.
 
 ########################
 ####  R Studio Tour #### 
@@ -32,7 +33,7 @@
 ####  Objects and data types
 ####
 
-# Create values with '<-' (or '=')
+# Create objects with '<-'
 n <- 5
 n2 <- 10
 
@@ -40,10 +41,15 @@ n
 
 # Vectors (set of values in one "line")
 n_vec <- c(1, 2, 3, 4, 5) 
+n_vec
 n_vec2 <- c(1:5)
+n_vec2
 
-species <- c("eagle", "snake", "trout")
+species <- c("eagle", "snake", "trout", "turtle", "squid")
+species
 
+# # class() allows you to determine what type of object something is
+# # This is helpful for troubleshooting errors
 class(n_vec)
 class(species)
 
@@ -53,18 +59,19 @@ class(n_vals)
 class(n_chars)
 
 # Matrices and dataframes
-# A matrix is a set of rows and columns of only one data type (typically numbers)
-# A dataframe can hold all data types (e.g, numbers, characters, factors)
+# # A matrix is a set of rows and columns of only one data type (typically numbers)
+# # A dataframe can hold all data types (e.g, numbers, characters, factors)
 
-# cbind() and rbind() combine vectors into matrices
+# cbind(), rbind(), and data.frame() combine vectors into matrices
 n_mat_c <- cbind(n_vec, n_vec2)
 n_mat_c
-class(n_mat)
+class(n_mat_c)
+
 n_mat_r <- rbind(n_vec, n_vec2)
 n_mat_r
+class(n_mat_r)
 
-# data.frame() combines vectors into dataframes
-n_df <- data.frame(n_vec, n_vec2)
+n_df <- data.frame(n_vec, species)
 n_df
 class(n_df)
 
@@ -81,7 +88,7 @@ seq(from=3, to=7, by=0.5)
 ####  Basic operations
 ####
 
-# R is just a big calculator, but can use both objects and numbers in calculations
+# R is just a big calculator, it can use objects and numbers in calculations
 N <- n + n2
 N 
 
@@ -97,29 +104,32 @@ species/2
 ####  Referencing elements / indexing
 ####
 
-#  [i] references the ith element of an object
+# [i] references the ith element of an object
 n_vec[1]
 species[2]
 
-# Matrices and dataframes are indexed by [rows,columns]
-n_mat_c[1,2]
-n_mat_c[1,]
-n_mat_c[,2]
+# Matrices and dataframes are indexed by [rows, columns]
+n_df[1,2]
+n_df[1,]
+n_df[,2]
 
-# One perk of using lebeled dataframes is calling columns using $
-n_df$n_vec
-class(n_df)
-class(n_df$n_vec)
+# One perk of using labeled dataframes is calling columns using $
+n_df$species
 
 # Let's go 3D: arrays (stack of matrices/datframes)
+# # Start by making three matrices to stack
 m1 <- matrix(data=rep(1,4),nrow=2,ncol=2)
 m2 <- matrix(data=rep(2,4),nrow=2,ncol=2)
 m3 <- matrix(data=rep(3,4),nrow=2,ncol=2)
+# # Then combine them into an array
 a <- array(data=c(m1,m2,m3),dim=c(2,2,3))
 
 a
-a[,,1]
+# # one "sheet" of the array
+a[,,1] 
+# # first column across all sheets
 a[,1,]
+# # first row across all sheets
 a[1,,]
 
 #####################
@@ -129,11 +139,12 @@ a[1,,]
 ####
 ####   
 ####
-# 1. Calculate your age in dog years using named objects (7 dog years per 1 human year).
+# 1. Calculate your age in dog years using a named object for your age (7 dog years per 1 human year).
+age <- xxx
 
 # 2. Create a vector that contains numeric and character elements. What happens?
 
-# 3. Create vectors of your top 3 favorite species and turn them into a matrix
+# 3. Create vectors of 3 birds, 3 mammals, and 3 fish, and turn them into a matrix
 
 # 4. How do you add row names to a dataframe within the data.frame() function?
 
--- Intermediate/looping.R
+++ Intermediate/looping.R
@@ -5,10 +5,13 @@
 
 ########################
 # Notes:
-#   1. Rule of thumb: if you repeat the same function/line of code >3 times, consider using a looping function
-#   2. 
-#   3.
-
+#   1. If you repeat the same function/line of code >3 times, consider using a looping function
+#   2. Start VERY BASIC when creating a loop to check what it's doing, then build up from there
+#   3. Common mistakes in for loops include:
+#      - Missing brackets
+#      - Not setting up your sequence correctly
+#      - Not using your index letting/the correct index letter in your loop
+#      - Not saving your results into a vector/matrix
 
 ########################
 ####  Demonstration #### 
@@ -20,15 +23,15 @@
 
 # Repeat the same line of code as many times as you specify
 for(i in 1:10){
-  print(i) # print() just prints what's inside in the console
+  print(i)
 }
 
-# vector <- numeric(LENGTH OF SEQUENCE)
-# for(INDEX in SEQUENCE){ #start loop
-#   vector[INDEX] <- FUNCTION USING INDEX
-# } #end loop
+# Breaking down the structure 
+for(i in 1:10){ # for every INDEX in SEQUENCE OF NUMBERS
+  print(i) # RUN a function or operation
+} # will loop over everything witin {}
 
-# This is usful when you want to automatatically update a vector
+# Loops are usful when you want to automatatically update a vector
 v <- numeric(10) # create a vector of 10 0's
 v
 for(i in 1:10){
@@ -45,8 +48,8 @@ running.mean
 
 # Loops are useful for random walks!
 xy <- matrix(data=0,nrow=10,ncol=2) #columns = coordinates
-for(t in 2:10){
-  xy[t,] <- rnorm(n=2,mean=xy[t-1,],sd=0.5)
+for(t in 2:10){ # start at 2, because we sample based on the last point
+  xy[t,] <- rnorm(n=2, mean=xy[t-1,], sd=0.5) #random draw from the normal distribution
 }
 
 plot(xy[,1],xy[,2])
@@ -58,13 +61,13 @@ lines(xy[,1],xy[,2])
 
 # Similar to for loops, but condensed code and a bit less flexible
 
-# apply() is used for matrices
-# apply(OBJECT, MARGIN (1=row, 2=colum), FUNCTION)
+# # apply() is used for matrices
+# # apply(OBJECT, MARGIN (1=row, 2=colum), FUNCTION)
 mat <- matrix(data=c(rep(1,4),rep(2,4),rep(3,4)),nrow=4,ncol=3)
 apply(mat, 1, sum)
 apply(mat, 2, prod)
 
-# sapply() can be used for matrices, vectors, or arrays
+# # sapply() can be used for matrices, vectors, or arrays
 vec <- 1:10
 sapply(vec, function(x){x+1})
 
@@ -78,9 +81,9 @@ for(1:10){
   y[i] <- mean(1:i)
 }
 
-# 2.) Sum each the column in the following matrix:
+# 2.) Sum each of the columns in the following matrix:
 prac <- matrix(rnorm(20,10,4),4,5)
 
-# 3.) Create a vector where each value is the previous value squared, starting with 2.
-
+# 3.) Create a vector of 5 numbers where each value is the previous value squared, starting with 2.
+# (Hint: use a for loop!)
 
